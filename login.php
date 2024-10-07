beb907070cb8f5c469f743c5f3dbfc77e0da2d09
kobedens <kobe.dens@hotmail.com>
2024-10-07 15:54:07 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+
+$correct_username = "admin";
+$correct_password = "password123";
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username === $correct_username && $password === $correct_password) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+
+        header('Location: Homepage.html');
+        exit;
+    } else {
+        echo "Invalid username or password!";
+    }
+}
+?>
