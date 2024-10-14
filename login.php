bfc413ca561b8c95e858a72e8b8bdac9e5994bde
kobedens <kobe.dens@hotmail.com>
2024-10-14 16:51:19 +0200

    Update login.php

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
 
-        header('Location: Homepage.html');
+        header('Location: House.html');
         exit;
     } else {
         echo "Invalid username or password!";
